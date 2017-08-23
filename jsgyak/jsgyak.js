353a59c88c570ad5eba64c33457bd76a69448e00
Anna <pataki.annamaria@hotmail.com>
2017-08-24 01:25:54 +0200

    added js fájl

---
+++ jsgyak/jsgyak.js
@@ -0,0 +1,10 @@
+var array = ['marvel', 'dc', 'netflix', 'bbc', 'hbo'];	//for előtt muszáj fent declarálni azt amit vizsgálok, mert különben undefined
+
+var upper = '';
+
+for (i = 0; i<array.length; i++){
+	upper = upper + array[i].toUpperCase() + ', ';
+	}
+
+
+alert(upper);
